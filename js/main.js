687168bbf2469503901f75cdbd11adae20629f34
Andrei2348 <makarevichandco@gmail.com>
2023-09-27 18:22:22 +0300

    Готовая версия сайта про байк

---
+++ js/main.js
@@ -0,0 +1,27 @@
+﻿$(function(){
+  $('.bike-slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true
+  });
+
+  $('.slider__items').slick({
+    arrows: false,
+    dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true
+  });
+
+  // Плавный скролл к якорю
+  $(".menu__list, .bike__column").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
